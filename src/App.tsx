1e4b725996517793766ffecd608a758483c14720
Alexandr Stepaniuk <Akeley1994@gmail.com>
2021-04-03 19:20:12 +0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AuthSDK, { TLoginBody, TUser } from "expample-sdk";
 
 function App() {
+  const [user, setUser] = useState<TUser | null>(null);
+  const [err, setError] = useState<string | null>(null);
+  const [form, setForm] = useState<TLoginBody>({
+    email: "",
+    password: "",
+  });
+  const onLogin = () => {
+    AuthSDK.login(form);
+  };
+
+  const onChage = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm((prevState) => ({ ...prevState, [e.target.id]: e.target.value }));
+  };
+
+  useEffect(() => {
+    AuthSDK.user((data) => {
+      setError(null);
+      setUser(data);
+    });
+    AuthSDK.error((err) => {
+      setUser(null);
+      setError(err);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={onChage}
+        id="email"
+        type="email"
+        placeholder="Email"
+        value={form.email}
+      />
+      <input
+        onChange={onChage}
+        id="password"
+        type="password"
+        placeholder="Password"
+        value={form.password}
+      />
+      <button onClick={onLogin}>Login</button>
+      {user && <p style={{ color: "green" }}>{JSON.stringify(user)}</p>}
+      {err && <p style={{ color: "red" }}>{JSON.stringify(err)}</p>}
     </div>
   );
 }
